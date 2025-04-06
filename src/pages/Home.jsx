acc1ffe5ffb0a270329a546c6e8378d506946ae5
Karolis Cibiras <83600971+Jeshki@users.noreply.github.com>
2025-04-06 15:33:55 +0300

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,8 +21,7 @@ function Home({ content }) {
              <a
                  href="/about"
                  // <<< Žalios temos mygtuko spalvos >>>
-                 className="mt-8 px-6 py-2 inline-flex items-center gap-2 bg-green-600 hover:bg-green-700 text-white rounded-lg dark:bg-green-500 dark:hover:bg-green-600 transition duration-300 shadow hover:shadow-md uppercase"
-             >
+                 className="mt-8 px-6 py-2 inline-flex items-center gap-2 bg-green-600 hover:bg-green-700 text-white rounded-lg dark:bg-green-500 dark:hover:bg-green-600 transition duration-300 shadow hover:shadow-md uppercase">
                  Sužinoti daugiau
                  <FaArrowRight />
              </a>
